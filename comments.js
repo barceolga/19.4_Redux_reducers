933d26f73ed5ff9b4b7c7262d393292b7687923e
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-06-23 13:18:49 +0200

    Fix bugs and update content ofr comments.js file

--- comments.js
+++ comments.js
@@ -5,48 +5,34 @@ import {REMOVE_COMMENT} from './actions';
 import {EDIT_COMMENT} from './actions';
 import {THUMB_UP_COMMENT} from './actions';
 import {THUMB_DOWN_COMMENT} from './actions';
+import addComment from './actions';
+import removeComment from './actions';
+import editComment from './actions';
+import thumbUpComment from './actions';
+import thumbDownComment from './actions';
 
 // Creating a reducer for administrating comments' state
 
-let comments = function comments (state = [], action) {
-  swith(action.type) {
-    case ADD_COMMENT:
-        return  [
-            {
-                id: action.id,
-                text: action.text
-                votes: 0
-            }
-          , ...state.comments]
-        })
-  })
-  case REMOVE_COMMENT:
-      return {
-          comments: state.comments.filter(comment => comment.id !== action.id)
-      };
-  case EDIT_COMMENT:
-      return {
-          comments: state.comments.find(comment => comment.id === action.id),
-          comment: {
-            text: action.text,
-          }
-        };
-  case THUMB_UP_COMMENT:
-      return {
-          comments: state.comments.find(comment => comment.id === action.id),
-          comment: {
-            votes: +1
-          }
-        };
-  case THUMB_DOWN_COMMENT:
-      return {
-          comments: state.comments.find(comment => comment.id === action.id),
-          comment: {
-            votes: -1
-          }
-        };
-  default:
-      return state;
-}
+export default function comments (state = [], action) {
+    switch(action.type) {
+      case ADD_COMMENT:
+          return  [
+              {
+                  id: action.id,
+                  text: action.text,
+                  votes: 0
+              }
+            , ...state.comments]
+    case REMOVE_COMMENT:
+        return  state.comments.filter(comment => comment.id !== action.id);
+    case EDIT_COMMENT:
+        return  state.comments.find(comment => comment.id === action.id).concat([comment.id, action.text]);
+    case THUMB_UP_COMMENT:
+        return comments: state.comments.find(comment => comment.id === action.id).concat([comment.id, action.votes +1]);
 
-export let comments;
+    case THUMB_DOWN_COMMENT:
+        return comments: state.comments.find(comment => comment.id === action.id).concat([comment.id,action.votes -1]);
+    default:
+        return state;
+    }
+}
