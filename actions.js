594630d8fbd3f6e24d205e750a3bc12c482a7b49
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-06-24 11:42:15 +0200

    Fix bugs regarding use of const in actions.js file

--- actions.js
+++ actions.js
@@ -1,56 +1,46 @@
-
-import uuid from uuid;
+/*jshint esversion: 6*/
+import uuid from 'uuid';
 
 //Examples of types of actions and actions' creators  in Redux
-const ADD_COMMENT = 'ADD_COMMENT';
-
-export function addComment(text) {
-  return {
-    type: ADD_COMMENT,
-    text,
-    id: uuid.v4()
-  }
-}
-
-const REMOVE_COMMENT = 'REMOVE_COMMENT';
-
-export function removeComment(id) {
-  return {
-    type: REMOVE_COMMENT,
-    id: comment.id
-  }
-}
-
-const EDIT_COMMENT = 'EDIT_COMMENT';
-
-export function editComment(id, text) {
-  return {
-    type: EDIT_COMMENT,
-    text,
-    id: comment.id
-  }
-}
-
-const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
-
-export function thumbUpComment(votes) {
-  return {
-    type: THUMB_UP_COMMENT.
-    id: comment.id
-  }
-}
-
-const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
-
-export function thumbDownComment(votes) {
-  return {
-    type: THUMB_DOWN_COMMENT,
-    id: comment.id
-  }
-}
-
 export const ADD_COMMENT = 'ADD_COMMENT';
 export const REMOVE_COMMENT = 'REMOVE_COMMENT';
 export const EDIT_COMMENT = 'EDIT_COMMENT';
 export const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
 export const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
+
+function addComment(text) {
+  return {
+    type: ADD_COMMENT,
+    text,
+    id: uuid.v4()
+  };
+}
+
+ function removeComment(id) {
+  return {
+    type: REMOVE_COMMENT,
+    id: comment.id
+  };
+}
+
+function editComment(id, text) {
+  return {
+    type: EDIT_COMMENT,
+    text,
+    id: comment.id
+  };
+}
+
+ function thumbUpComment(votes) {
+  return {
+    type: THUMB_UP_COMMENT,
+    id: comment.id
+  };
+}
+
+ function thumbDownComment(votes) {
+  return {
+    type: THUMB_DOWN_COMMENT,
+    id: comment.id
+  };
+}
