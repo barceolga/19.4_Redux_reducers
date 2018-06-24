bf01c574611dceaa54374e272cfc3f55ed259e7a
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-06-24 11:43:53 +0200

    Delete the unnecessary const initialState in reducers.js file

--- reducers.js
+++ reducers.js
@@ -1,15 +1,10 @@
+
+/*jshint esversion: 6*/
 //Importing small reducers from other files and importing an auxiliar funtion comineReducers for combing small reducers into main reducer
 
 import {combineReducers} from 'redux';
 import comments from './comments';
 import users from './users';
-
-//Defining an initial state
-
-const initialState = {
-  comments: [],
-  users: []
-}
 
 // Main Reducer
 
